e929092af20f943d9ad52ddab97f43eaca3386e7
巫镇泉 <wuzhenquan1990@gmail.com>
2019-09-18 11:47:11 +0800

    update

---
+++ 前端进阶/2 throttle/4 定时器版.js
@@ -0,0 +1,16 @@
+function throttle(func, wait) {
+    var timeout;
+    var previous = 0;
+
+    return function() {
+        context = this;
+        args = arguments;
+        if (!timeout) {
+            timeout = setTimeout(function(){
+                timeout = null;
+                func.apply(context, args)
+            }, wait)
+        }
+
+    }
+}
